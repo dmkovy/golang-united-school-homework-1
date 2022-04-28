2af7ad75b0ac8ad2ef35e2c58243a55d82ffa0d3
Dm <dmkhost@gmail.com>
2022-04-28 19:02:31 +0200

    add solution

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/dmkovy/golang-united-school-homework-1/tree/lecture-00
+
+go 1.17
+
+require github.com/kyokomi/emoji/v2 v2.2.9 // indirect
--- solution.go
+++ solution.go
@@ -1,5 +1,8 @@
 package solution
 
+import "github.com/kyokomi/emoji/v2"
+
 func GetMessage() string {
-	return ""
+	hello := emoji.Sprint("Hello :world_map:")
+	return hello
 }
